d15efda1a10642f1dddef04d170bc3871cb6c108
Veronica Lopez <nicaa0695@gmail.com>
2019-08-14 17:40:56 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,8 +13,8 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT tardies COUNT(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
